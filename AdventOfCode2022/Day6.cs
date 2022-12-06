e0916a17ff34787f1e04f905298a8b41702f9da2
Robert Massa <robertmassa@gmail.com>
2022-12-06 08:31:53 +0100

    Day 6

---
+++ AdventOfCode2022/Day6.cs
@@ -0,0 +1,14 @@
+﻿using MoreLinq;
+
+namespace AdventOfCode2022;
+
+public static class Day6 {
+    public static object Run1(int size = 4) { // 1848
+        var signal = File.ReadAllText("day6.txt");
+        var marker = signal.WindowLeft(size).First(x => x.Distinct().Exactly(size));
+
+        return signal.IndexOf(new string(marker.ToArray())) + size;
+    }
+
+    public static object Run2() => Run1(size: 14); // 2308
+}
--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,1 +1,2 @@
-﻿Day2.Run2().Dump();
+﻿Day6.Run1().Dump();
+Day6.Run2().Dump();
