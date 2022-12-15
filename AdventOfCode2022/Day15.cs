8ea71a8d7497f8318ff632da4f9eec7d367fd3cf
Robert Massa <robertmassa@gmail.com>
2022-12-16 00:27:45 +0100

    Fix regression

--- AdventOfCode2022/Day15.cs
+++ AdventOfCode2022/Day15.cs
@@ -6,10 +6,10 @@ namespace AdventOfCode2022 {
     public static class Day15 {
         private static (Vec sensor, Vec beacon, int dist)[] ParseInput() =>
             (from line in File.ReadAllLines("day15.txt")
-             let x = Regex.Matches(line, @"-?\d+").Select(n => int.Parse(n.Value)).ToArray()
-             let sensor = new Vec(x[0], x[1])
-             let beacon = new Vec(x[2], x[3])
-             select (sensor, beacon, dist: (beacon - sensor).Length)).ToArray();
+                    let x = Regex.Matches(line, @"-?\d+").Select(n => int.Parse(n.Value)).ToArray()
+                    let sensor = new Vec(x[0], x[1])
+                    let beacon = new Vec(x[2], x[3])
+                    select (sensor, beacon, dist: (beacon - sensor).Length)).ToArray();
 
         public static object Run1() {
             var items = ParseInput();
@@ -17,7 +17,7 @@ namespace AdventOfCode2022 {
 
             return (from x in Range(min, max - min).AsParallel()
                     let pos = new Vec(x, 2000000)
-                    select items.Any(i => i.beacon == pos || pos.Distance(i.sensor) <= i.dist) ? 1 : 0).Sum();
+                    select (items.Any(i => pos.Distance(i.sensor) <= i.dist) && items.All(i => i.beacon != pos)) ? 1 : 0).Sum();
         }
 
         public static object Run2() {
@@ -32,7 +32,7 @@ namespace AdventOfCode2022 {
                          let interval = new Interval(item.sensor.X - width, item.sensor.X + width + 1)
                          orderby interval.Start
                          select interval)
-                        .Scan((a, b) => b with { Start = Math.Max(a.End, b.Start) })
+                        .Scan((a, b) => new Interval(Math.Max(a.End, b.Start), b.End))
                         .Pairwise((a, b) => (a.End, b.Start))
                         .FirstOrDefault(x => x.End != x.Start).End;
 
